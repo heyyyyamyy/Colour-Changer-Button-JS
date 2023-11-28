77437b07afd1b638ac85fb3200ea051eb33439e3
heyyyyamyy <cruisesufiyan2@gmail.com>
2023-11-28 17:14:58 +0530

    first commit

---
+++ First.js
@@ -0,0 +1,15 @@
+function btnblack(){
+    document.body.style.backgroundColor = "black"
+}
+
+function btnwhite(){
+    document.body.style.backgroundColor = "white"
+}
+
+function btnyellow(){
+    document.body.style.backgroundColor = "yellow"
+}
+
+function btngreen(){
+    document.body.style.backgroundColor = "green"
+}
